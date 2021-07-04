f81d365eb1a9783b6d708208f6eb3d83f2c89ef7
xiaobie123 <1018677364@qq.com>
2021-07-04 19:37:01 +0800

    init

---
+++ comTaluo/index.js
@@ -0,0 +1,89 @@
+import da_0 from './da-0.jpg';
+import da_1 from './da-1.jpg';
+import da_2 from './da-2.jpg';
+import da_3 from './da-3.jpg';
+import da_4 from './da-4.jpg';
+import da_5 from './da-5.jpg';
+import da_6 from './da-6.jpg';
+import da_7 from './da-7.jpg';
+import da_8 from './da-8.jpg';
+import da_9 from './da-9.jpg';
+import da_10 from './da-10.jpg';
+import da_11 from './da-11.jpg';
+import da_12 from './da-12.jpg';
+import da_13 from './da-13.jpg';
+import da_14 from './da-14.jpg';
+import da_15 from './da-15.jpg';
+import da_16 from './da-16.jpg';
+import da_17 from './da-17.jpg';
+import da_18 from './da-18.jpg';
+import da_19 from './da-19.jpg';
+import da_20 from './da-20.jpg';
+import da_21 from './da-21.jpg';
+import bz_1 from './bz-1.jpg';
+import bz_2 from './bz-2.jpg';
+import bz_3 from './bz-3.jpg';
+import bz_4 from './bz-4.jpg';
+import bz_5 from './bz-5.jpg';
+import bz_6 from './bz-6.jpg';
+import bz_7 from './bz-7.jpg';
+import bz_8 from './bz-8.jpg';
+import bz_9 from './bz-9.jpg';
+import bz_10 from './bz-10.jpg';
+import bz_11 from './bz-11.jpg';
+import bz_12 from './bz-12.jpg';
+import bz_13 from './bz-13.jpg';
+import bz_14 from './bz-14.jpg';
+import qz_1 from './qz-1.jpg';
+import qz_2 from './qz-2.jpg';
+import qz_3 from './qz-3.jpg';
+import qz_4 from './qz-4.jpg';
+import qz_5 from './qz-5.jpg';
+import qz_6 from './qz-6.jpg';
+import qz_7 from './qz-7.jpg';
+import qz_8 from './qz-8.jpg';
+import qz_9 from './qz-9.jpg';
+import qz_10 from './qz-10.jpg';
+import qz_11 from './qz-11.jpg';
+import qz_12 from './qz-12.jpg';
+import qz_13 from './qz-13.jpg';
+import qz_14 from './qz-14.jpg';
+import xb_1 from './xb-1.jpg';
+import xb_2 from './xb-2.jpg';
+import xb_3 from './xb-3.jpg';
+import xb_4 from './xb-4.jpg';
+import xb_5 from './xb-5.jpg';
+import xb_6 from './xb-6.jpg';
+import xb_7 from './xb-7.jpg';
+import xb_8 from './xb-8.jpg';
+import xb_9 from './xb-9.jpg';
+import xb_10 from './xb-10.jpg';
+import xb_11 from './xb-11.jpg';
+import xb_12 from './xb-12.jpg';
+import xb_13 from './xb-13.jpg';
+import xb_14 from './xb-14.jpg';
+import sb_1 from './sb-1.jpg';
+import sb_2 from './sb-2.jpg';
+import sb_3 from './sb-3.jpg';
+import sb_4 from './sb-4.jpg';
+import sb_5 from './sb-5.jpg';
+import sb_6 from './sb-6.jpg';
+import sb_7 from './sb-7.jpg';
+import sb_8 from './sb-8.jpg';
+import sb_9 from './sb-9.jpg';
+import sb_10 from './sb-10.jpg';
+import sb_11 from './sb-11.jpg';
+import sb_12 from './sb-12.jpg';
+import sb_13 from './sb-13.jpg';
+import sb_14 from './sb-14.jpg';
+
+export default {
+  da_0,
+  da_1,da_2,da_3,da_4,da_5,da_6,da_7,da_8,da_9,da_10,
+  da_11,da_12,da_13,da_14,da_15,da_16,da_17,da_18,da_19,da_20,
+  da_21,
+  sb_1,sb_2,sb_3,sb_4,sb_5,sb_6,sb_7,sb_8,sb_9,sb_10,sb_11,sb_12,sb_13,sb_14,
+  xb_1,xb_2,xb_3,xb_4,xb_5,xb_6,xb_7,xb_8,xb_9,xb_10,xb_11,xb_12,xb_13,xb_14,
+  qz_1,qz_2,qz_3,qz_4,qz_5,qz_6,qz_7,qz_8,qz_9,qz_10,qz_11,qz_12,qz_13,qz_14,
+  bz_1,bz_2,bz_3,bz_4,bz_5,bz_6,bz_7,bz_8,bz_9,bz_10,bz_11,bz_12,bz_13,bz_14,
+}
